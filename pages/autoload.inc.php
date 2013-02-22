5e54226a6b7092e2e1be4fcf19e868cf8e93bdcc
tbaddade <thomas@redaxo.de>
2013-02-22 01:07:30 +0100

    Kommentar hinzugefügt

--- pages/autoload.inc.php
+++ pages/autoload.inc.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ *
+ * @author blumbeet - web.studio
+ * @author Thomas Blum
+ * @author mail[at]blumbeet[dot]com Thomas Blum
+ *
+ */
+
 $func = rex_request('func', 'string');
 
 $mypage = 'redaxo5';
